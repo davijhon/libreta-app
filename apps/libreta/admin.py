1c69a34859b9bb4079c50fe7aa799518d2570d9e
Dave <davidalejandrodand@gmail.com>
2023-03-23 08:45:30 -0400

    Admin panel for django-admin

--- apps/libreta/admin.py
+++ apps/libreta/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+admin.site.register(Direccion)
+
+
+@admin.register(Persona)
+class PersonaAdmin(admin.ModelAdmin):
+	list_display = [
+			'dni',
+			'nombre', 
+			'apellido', 
+			'edad'
+	]
+	list_filter = ['dni', 'nombre', 'apellido']
+	list_editable = ('edad',)
