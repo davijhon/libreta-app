63b68d57eb1263381675ba3b0830c580d88d3b4e
Dave <davidalejandrodand@gmail.com>
2023-03-23 08:46:39 -0400

    Remove urls (Replaced for Routers)

---
+++ apps/libreta/api/urls.py
@@ -0,0 +1,10 @@
+# from django.urls import path
+
+# from apps.libreta.api.views import DireccionView, ListCreatePersonaView, DetailPersonaView
+
+
+# urlpatterns = [
+#     path('personas/<int:pk>/', DetailPersonaView.as_view(), name='persona'),
+#     path('personas/', ListCreatePersonaView.as_view(), name='persona_list'),
+#     path('direcciones/', DireccionView.as_view(), name = 'direccion_persona'),    
+# ]
